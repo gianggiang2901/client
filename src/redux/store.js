20b70cb318f0646c83639831187faa9c4396cdc2
Cẩm Giang <gianggiang29290101@gmail.com>
2023-01-28 17:20:52 +0700

    Add new project client

---
+++ src/redux/store.js
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import themeModeSlice from "./features/themeModeSlice";
+import userSlice from "./features/userSlice";
+import appStateSlice from "./features/appStateSlice";
+import authModalSlice from "./features/authModalSlice";
+import globalLoadingSlice from "./features/globalLoadingSlice";
+
+const store = configureStore({
+  reducer: {
+    user: userSlice,
+    themeMode: themeModeSlice,
+    authModal: authModalSlice,
+    globalLoading: globalLoadingSlice,
+    appState: appStateSlice,
+  },
+});
+export default store;
